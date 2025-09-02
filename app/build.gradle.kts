0b2b782ddd509cd87e72d0c0aab0a959a39a0f32
Nicolas Raoul <nicolas.raoul@gmail.com>
2025-09-02 22:35:39 +0900

    Release 1.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.nicolasraoul.rosette"
         minSdk = 34
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.9"
+        versionCode = 12
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
